67fe88956bfeb7c1621bc3fd2e2475bf74b61ec0
Márkus János Dániel <aresius423@gmail.com>
2019-02-03 10:22:40 +0100

    tabs added

--- js/index.js
+++ js/index.js
@@ -5,6 +5,23 @@ var plumb = null;
 var $parent = $(".parent");
 $diagram = $parent.find(".diagram");
 
+function openTab(evt, tabid) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabid).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 (function() {
           var $section = $('#focal');
           var $panzoom = $section.find('.panzoom').panzoom();
